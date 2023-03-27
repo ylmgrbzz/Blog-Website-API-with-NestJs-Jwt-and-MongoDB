41c1b03d5b8a2fa360980434d8ce20027b11af40
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-27 11:44:01 +0300

    BlogWebsiteAPI

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
+
     @Post('register')
-    register() {
-        return this.authService.register();
+    register(@Body() dto: AuthDto) {
+        return this.authService.register(dto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/users.schema';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-    register() {
-        return 'Register';
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) { }
+
+    async register(dto: AuthDto) {
+        const newUser = new this.userModel({
+            email: dto.email,
+            password: dto.password,
+        })
+
+        return await newUser.save();
     }
 }
