e16f58557695854c469ee0c942876b349cf47b89
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-27 11:27:47 +0300

    BlogWebsiteAPI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/blog'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
