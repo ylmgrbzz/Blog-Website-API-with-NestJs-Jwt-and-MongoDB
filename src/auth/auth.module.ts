879e9eb4b77cf7765c1504b890e46d535232909a
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-27 11:51:57 +0300

    BlogWebsiteAPI

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/users.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  JwtModule.register({
+    secret: ' cat',
+    signOptions: { expiresIn: '20m' },
+  })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,16 +3,20 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/users.schema';
 import { AuthDto } from './dto/auth.dto';
-
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) { }
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>,
+        private jwtService: JwtService,
+    ) { }
 
     async register(dto: AuthDto) {
+        const hashedPassword = await bcrypt.hash(dto.password, 15);
         const newUser = new this.userModel({
             email: dto.email,
-            password: dto.password,
-        })
+            password: hashedPassword,
+        });
 
         return await newUser.save();
     }
