868881940bf98542acc21749180decf5f541f024
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-27 13:28:53 +0300

    BlogWebsiteAPI

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -1,13 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+    Request,
+    UseGuards,
+} from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { BlogDto } from './dto/blog.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Blog } from './schemas/blog.schema';
 
 @Controller('blogs')
 export class BlogsController {
+    [x: string]: any;
     constructor(private BlogService: BlogsService) { }
 
+    @UseGuards(AuthGuard('jwt'))
     @Post()
-    createBlog(@Body() dto: BlogDto) {
-        return this.BlogService.createBlog(dto);
+    @Post()
+    createBlog(@Request() req: any, @Body() dto: BlogDto): Promise<Blog> {
+        return this.blogService.createBlog(req, dto);
     }
 }
--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose/dist';
 import { Blog, BlogDocument } from './schemas/blog.schema';
 import { Model } from 'mongoose';
@@ -11,12 +11,42 @@ export class BlogsService {
         @InjectModel(Blog.name) private blogModel: Model<BlogDocument>,
         @InjectModel(User.name) private userModel: Model<UserDocument>,
     ) { }
-    async createBlog(dto: BlogDto) {
+    async createBlog(req: any, dto: BlogDto): Promise<Blog> {
+        const currentUser = await this.userModel.findById(req.user.id);
         const newbBlog = new this.blogModel({
             title: dto.title,
             content: dto.content,
+            sharedBy: currentUser.email,
+            userId: currentUser._id,
         });
         const blog = await newbBlog.save();
         return blog;
+    }
+    async updateBlog(id: any, dto: BlogDto): Promise<Blog> {
+        const blog = await this.blogModel.findById(id);
+        if (dto.userId !== blog.userId) {
+            throw new UnauthorizedException('You can update only your blogs!');
+        }
+        return await this.blogModel.findByIdAndUpdate(id, dto, { new: true });
+    }
+
+    async deleteBlog(id: any, dto: BlogDto): Promise<Blog> {
+        const blog = await this.blogModel.findById(id);
+        if (dto.userId !== blog.userId) {
+            throw new UnauthorizedException('You can delete only your blogs!');
+        }
+        return await this.blogModel.findByIdAndRemove(id);
+    }
+
+    async getCurrentUserBlogs(req: any): Promise<Blog[]> {
+        return await this.blogModel.find({ userId: req.user.id });
+    }
+
+    async getAllBlogs(): Promise<Blog[]> {
+        return await this.blogModel.find();
+    }
+
+    async getOneBlog(id: any): Promise<Blog> {
+        return await this.blogModel.findById(id);
     }
 }
