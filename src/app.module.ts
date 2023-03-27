83215bbb6df5af7d420e9d835461ec138968196f
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-27 13:55:22 +0300

    BlogWebsiteAPI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { BlogsModule } from './blogs/blogs.module';
+import { config } from './config/env.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('blog'),
+      }),
+    }),
     AuthModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/blog'),
     BlogsModule,
   ],
   controllers: [AppController],
