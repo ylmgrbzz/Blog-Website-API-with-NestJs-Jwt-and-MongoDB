7280989eb3538036d02caddb14475fd700713290
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-27 13:39:00 +0300

    BlogWebsiteAPI

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -25,4 +25,31 @@ export class BlogsController {
     createBlog(@Request() req: any, @Body() dto: BlogDto): Promise<Blog> {
         return this.blogService.createBlog(req, dto);
     }
+    @UseGuards(AuthGuard('jwt'))
+    @Put(':id')
+    updateBlog(@Param('id') id: any, @Body() dto: BlogDto): Promise<Blog> {
+        return this.blogService.updateBlog(id, dto);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Delete(':id')
+    deleteBlog(@Param('id') id: any, @Body() dto: BlogDto): Promise<Blog> {
+        return this.blogService.deleteBlog(id, dto);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get('my-blogs')
+    getCurrentUserBlogs(@Request() req: any): Promise<Blog[]> {
+        return this.blogService.getCurrentUserBlogs(req);
+    }
+
+    @Get()
+    getAllBlogs(): Promise<Blog[]> {
+        return this.blogService.getAllBlogs();
+    }
+
+    @Get(':id')
+    getOneBlog(@Param('id') id: any): Promise<Blog> {
+        return this.blogService.getOneBlog(id);
+    }
 }
